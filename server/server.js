661f99beec73cb846e750c51cd179bd9782a6f34
Krishna <krishnasaraf134@gmail.com>
2024-12-19 00:39:07 +0530

    Webhooks Added

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import express from 'express'
 import cors from 'cors'
 import { connect } from 'mongoose'
 import connectDB from './configs/mongodb.js'
+import userRouter from './routes/userRoutes.js'
 
 //App Config
 const PORT = 4000
@@ -16,5 +17,6 @@ app.use(cors())
 
 //API Routes
 app.get('/',(req,res)=> res.send("API Working"))
+app.use('/api/user',userRouter)
 
 app.listen(PORT,()=>console.log("Server Running on port"+PORT))
